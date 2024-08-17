db6645f604225f5c8be339e23d31d42c1e18c324
Alexander <alexandrov327027@yandex.ru>
2024-08-17 11:05:12 +0300

    create activities

---
+++ app/src/main/java/com/example/cryptocurrencytesttask/presentation/DetailInfoActivity.kt
@@ -0,0 +1,12 @@
+package com.example.cryptocurrencytesttask.presentation
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.example.cryptocurrencytesttask.R
+
+class DetailInfoActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_detail_info)
+    }
+}
